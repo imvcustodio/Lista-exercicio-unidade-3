c9bd96bc0b47c116c50710d883238a3b53e7681f
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-12 08:35:47 -0300

    exe16.java

--- exe16.java
+++ exe16.java
@@ -18,6 +18,13 @@ public class exe16 {
             300
             o minimo de notas que compoem o troco é 10
             o troco deu 46 e e compoto por 0 Notas de 100, 4 Notas de 10 e 6 moedas de 1
+
+      teste 2: Valor total da compra: 
+            1400
+            Valor total dado pelo cliente:
+            1650
+            o minimo de notas que compoem o troco é 7
+            o troco deu 250 e e compoto por 2 Notas de 100, 5 Notas de 10 e 0 moedas de 1
      */
     public static void main(String[] args) throws Exception {
         Scanner leitor = new Scanner(System.in);
